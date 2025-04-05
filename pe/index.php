ada59c68e5184722ca7341d15e5376cf28c77b57
couldflyer <100808lts@gmail.com>
2025-04-05 16:30:14 +0800

    自动提交于 2025-04-05 16:30:14

--- pe/index.php
+++ pe/index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 $randomNumber = rand(1, 258);
-$imageUrl = 'https://cdn.jsdelivr.net/gh/couldflyer/allimg@main/pc/img' . $randomNumber . '.webp';
+$imageUrl = 'https://cdn.jsdmirror.com/gh/couldflyer/allimg@main/pe/img' . $randomNumber . '.webp';
 
 // 使用file_get_contents替代
 function fetchImage($url) {
